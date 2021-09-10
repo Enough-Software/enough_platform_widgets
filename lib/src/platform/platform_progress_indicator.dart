cb2a48f368cada3ea2f64c7a8361a994eda7678f
Robert Virkus <robert.virkus@enough.de>
2021-09-10 12:00:22 +0200

    support progress in PlatformProgressIndicator

--- lib/src/platform/platform_progress_indicator.dart
+++ lib/src/platform/platform_progress_indicator.dart
@@ -1,16 +1,20 @@
+import 'package:cupertino_progress_bar/cupertino_progress_bar.dart';
 import 'package:enough_platform_widgets/enough_platform_widgets.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-/// Uses a `CircularProgressIndicator` on material and a `CupertinoActivityIndicator` on cupertino
+/// Uses a `CircularProgressIndicator` on material and a `CupertinoActivityIndicator` - or the 'CupertinoProgressBar' from the `cupertino_progress_bar` package when the given value is not null - on cupertino
 class PlatformProgressIndicator extends StatelessWidget {
-  const PlatformProgressIndicator({Key? key}) : super(key: key);
+  final double? value;
+  const PlatformProgressIndicator({Key? key, this.value}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    final v = value;
     return PlatformWidget(
-      material: (context, platform) => CircularProgressIndicator(),
-      cupertino: (context, platform) => CupertinoActivityIndicator(),
-    );
+        material: (context, platform) => CircularProgressIndicator(value: v),
+        cupertino: (context, platform) => (v == null)
+            ? CupertinoActivityIndicator()
+            : CupertinoProgressBar(value: v));
   }
 }
