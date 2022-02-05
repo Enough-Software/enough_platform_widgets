e7ce85cf1e1b922792364ebd004c0f32ad1c26b9
Robert Virkus <robert.virkus@enough.de>
2022-02-05 17:31:21 +0100

    add additional common platform icons

--- lib/src/platform/platform_icons.dart
+++ lib/src/platform/platform_icons.dart
@@ -31,4 +31,39 @@ class CommonPlatformIcons {
       _isCupertino ? CupertinoIcons.strikethrough : Icons.format_strikethrough;
 
   static IconData get info => _isCupertino ? CupertinoIcons.info : Icons.info;
+
+  static IconData get add => _isCupertino ? CupertinoIcons.add : Icons.add;
+
+  static IconData get copy =>
+      _isCupertino ? CupertinoIcons.rectangle_on_rectangle : Icons.copy;
+
+  static IconData get delete =>
+      _isCupertino ? CupertinoIcons.delete : Icons.delete;
+
+  static IconData get edit => _isCupertino ? CupertinoIcons.pencil : Icons.edit;
+
+  static IconData get send =>
+      _isCupertino ? CupertinoIcons.paperplane : Icons.send;
+
+  static IconData get settings =>
+      _isCupertino ? CupertinoIcons.settings : Icons.settings;
+
+  static IconData get refresh =>
+      _isCupertino ? CupertinoIcons.refresh : Icons.refresh;
+
+  static IconData get person =>
+      _isCupertino ? CupertinoIcons.person : Icons.person;
+
+  static IconData get group =>
+      _isCupertino ? CupertinoIcons.group : Icons.group;
+
+  static IconData get account =>
+      _isCupertino ? CupertinoIcons.person_circle : Icons.account_circle;
+
+  static IconData get mailUnread =>
+      _isCupertino ? CupertinoIcons.envelope_badge : Icons.mark_email_unread;
+
+  static IconData get mailRead => _isCupertino
+      ? CupertinoIcons.envelope_open
+      : Icons.mark_email_read_outlined;
 }
